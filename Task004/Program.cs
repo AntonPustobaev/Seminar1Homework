09c9f966883ee61d15d80caa9782b31269e1d7aa
Antonio <don_ne@mail.ru>
2022-04-16 12:16:13 +0400

    task4

---
+++ Task004/Program.cs
@@ -0,0 +1,23 @@
+﻿int a, b, c;
+
+int max;
+ 
+Console.WriteLine ("Введите числа: ");
+ 
+a = Convert.ToInt32(Console.ReadLine());
+ 
+b = Convert.ToInt32(Console.ReadLine());
+
+c = Convert.ToInt32(Console.ReadLine());
+
+max=a;
+
+if (b>max)
+{
+    Console.WriteLine($"max  {b}");
+}
+
+if (c>max)
+{
+    Console.WriteLine($"max  {c}");
+}
