5e53272193cfb82e6259d024bb792e7a6c57f8ca
Antonio <don_ne@mail.ru>
2022-04-16 11:34:33 +0400

    task002

---
+++ Task002/Program.cs
@@ -0,0 +1,18 @@
+﻿int a, b;
+ 
+Console.WriteLine ("Введите числа: ");
+ 
+a = Convert.ToInt32(Console.ReadLine());
+ 
+b = Convert.ToInt32(Console.ReadLine());
+ 
+if (a > b)
+{
+    Console.WriteLine($"max  {a}");
+    Console.WriteLine($"min  {b}");
+}
+else 
+{
+    Console.WriteLine($"max  {b}");
+    Console.WriteLine($"min  {a}");
+}
